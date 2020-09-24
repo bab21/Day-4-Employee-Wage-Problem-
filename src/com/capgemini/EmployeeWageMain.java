db1f398655fe1b9e5b043cfb584c32d5daa4da19
bab21 <2016061@iiitdmj.ac.in>
2020-09-25 00:51:21 +0530

    Checking employee is present or absent

--- src/com/capgemini/EmployeeWageMain.java
+++ src/com/capgemini/EmployeeWageMain.java
@@ -1,10 +1,20 @@
 package com.capgemini;
+import java.util.*;
+
 
 public class EmployeeWageMain {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 		
+		Random rand=new Random();
+		
+		//Checking for attendance....
+		int prs_abs=rand.nextInt(2);
+		
+		if(prs_abs==0)
+			System.out.println("Absent");
+		else System.out.println("Present");
 	}
 
 }
