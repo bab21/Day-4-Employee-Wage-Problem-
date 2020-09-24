6c6947476ff3188134e5949b3374cd177dccd035
bab21 <2016061@iiitdmj.ac.in>
2020-09-25 01:29:49 +0530

    calculating wages

--- src/com/capgemini/EmployeeWageMain.java
+++ src/com/capgemini/EmployeeWageMain.java
@@ -7,6 +7,8 @@ public class EmployeeWageMain {
 	public static final int FULL_DAY_HOUR=8;
 	public static final int HALF_DAY_HOUR=8;
 	public static final int WORKING_DAYS_PER_MONTH=20;
+	public static final int MAX_WORKING_HOURS=100;
+	
 	
 	
 	public static void main(String[] args) {
@@ -24,21 +26,29 @@ public class EmployeeWageMain {
 		int full_part=rand.nextInt(2);
 		int daily_wage=0;
 		int monthly_wage=0;
+		int total_working_hrs=0;
+		int day=0;
 		
-		for(int days=0;days<WORKING_DAYS_PER_MONTH;days++) {
+		
+		
+		while(day<WORKING_DAYS_PER_MONTH && total_working_hrs<MAX_WORKING_HOURS) {
 			
 		switch(full_part) {
 		case 1:daily_wage=WAGE_PER_HOUR*FULL_DAY_HOUR;
+			   total_working_hrs=total_working_hrs+8;
 			   break;
-		case 2:daily_wage=WAGE_PER_HOUR*HALF_DAY_HOUR;
+		case 0:daily_wage=WAGE_PER_HOUR*HALF_DAY_HOUR;
+		       total_working_hrs=total_working_hrs+4;
 				break;
 		default:System.out.print("nothing");
 		
 		}
 		monthly_wage=monthly_wage+daily_wage;
+		day++;
 		
 		}
 		
+		System.out.println("no of days"+day);
 		
 		
 		
