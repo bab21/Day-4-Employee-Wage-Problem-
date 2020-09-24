f599deab537bc89bd6f80680dfe0d82194756e18
bab21 <2016061@iiitdmj.ac.in>
2020-09-25 01:19:13 +0530

    Wages for a month

--- src/com/capgemini/EmployeeWageMain.java
+++ src/com/capgemini/EmployeeWageMain.java
@@ -6,6 +6,7 @@ public class EmployeeWageMain {
 	public static final int WAGE_PER_HOUR=20;
 	public static final int FULL_DAY_HOUR=8;
 	public static final int HALF_DAY_HOUR=8;
+	public static final int WORKING_DAYS_PER_MONTH=20;
 	
 	
 	public static void main(String[] args) {
@@ -22,6 +23,10 @@ public class EmployeeWageMain {
 		
 		int full_part=rand.nextInt(2);
 		int daily_wage=0;
+		int monthly_wage=0;
+		
+		for(int days=0;days<WORKING_DAYS_PER_MONTH;days++) {
+			
 		switch(full_part) {
 		case 1:daily_wage=WAGE_PER_HOUR*FULL_DAY_HOUR;
 			   break;
@@ -30,9 +35,14 @@ public class EmployeeWageMain {
 		default:System.out.print("nothing");
 		
 		}
+		monthly_wage=monthly_wage+daily_wage;
+		
+		}
 		
 		
-		System.out.println("Wage of employee for the day :"+daily_wage);
+		
+		
+		System.out.println("Wage of employee for the month :"+monthly_wage);
 		
 		
 		
