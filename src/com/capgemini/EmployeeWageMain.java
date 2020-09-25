62cb4e8fe5f7e02844e79f27e44e97dd26d944c5
bab21 <2016061@iiitdmj.ac.in>
2020-09-25 16:19:03 +0530

    total wage by company name calculated

--- src/com/capgemini/EmployeeWageMain.java
+++ src/com/capgemini/EmployeeWageMain.java
@@ -3,43 +3,42 @@ import java.util.*;
 
 
 public class EmployeeWageMain implements IComputeEmpWage {
-//	public static final int WAGE_PER_HOUR=20;
+
 	public static final int FULL_DAY_HOUR=8;
 	public static final int HALF_DAY_HOUR=4;
-//	public static final int WORKING_DAYS_PER_MONTH=20;
-//	public static final int MAX_WORKING_HOURS=100;
+
 	
 	private int numOfCompany=0;
-//	private int daily_wage;
+	private int daily_wage;
 	private ArrayList<CompanyEmpWage> companyEmpWageArray;
+	private Map<String,CompanyEmpWage> companyToEmployeeMap;
 	
-//	public void compute_daily_wage(int emp_type) {
-//		int ans=0;
-//		
-//		
-//		int emphrs=0;
-//		if(emp_type==0)
-//			emphrs=4;
-//		else emphrs=8;
-//		
-//		for(int i=0;i<numOfCompany;i++) {
-//			ans=ans+companyEmpWageArray.get(i).empRatePerHour*emphrs;
-//		}
-//		daily_wage=ans;
-//		
-//	}
-//	public int total_wage_of_company(int company_num) {
-//		
-//		return companyEmpWageArray.get(company_num).totalEmpWage;
-//		
-//	}
+	public void compute_daily_wage(int emp_type) {
+		int ans=0;
+		
+		
+		int emphrs=0;
+		if(emp_type==0)
+			emphrs=4;
+		else emphrs=8;
+		
+		for(int i=0;i<numOfCompany;i++) {
+			ans=ans+companyEmpWageArray.get(i).empRatePerHour*emphrs;
+		}
+		daily_wage=ans;
+		
+	}
+
 	
 	public EmployeeWageMain() {
 		companyEmpWageArray=new ArrayList<CompanyEmpWage>();
+		companyToEmployeeMap=new HashMap<>();
 	}
 	
 	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
-		companyEmpWageArray.add(new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth));
+		CompanyEmpWage obj=new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
+		companyEmpWageArray.add(obj);
+		companyToEmployeeMap.put(company,obj);
 		numOfCompany++;
 	}
 	
@@ -80,7 +79,9 @@ public class EmployeeWageMain implements IComputeEmpWage {
 	    
 	     
 	}
-	
+	public int getTotalWage(String company) {
+		return companyToEmployeeMap.get(company).totalEmpWage;
+	}
 	
 	
 	public static void main(String[] args) {
@@ -102,16 +103,12 @@ public class EmployeeWageMain implements IComputeEmpWage {
 		EmployeeWageMain mainobject=new EmployeeWageMain();
 		
 		mainobject.addCompanyEmpWage("google", 20, 2, 20);
-//		mainobject.compute_daily_wage(full_part);
-//		System.out.println("Employee Daily wage "+mainobject.daily_wage);
-		
 		mainobject.addCompanyEmpWage("deshaw", 50, 2, 60);
+		
+		mainobject.compute_daily_wage(full_part);
+		System.out.println("Employee Daily wage   ..... "+mainobject.daily_wage);
 		mainobject.computeEmpWage(full_part);
-		
-		int company_num=1;
-//		int total_wage_of_the_company=mainobject.total_wage_of_company(company_num);
-//		System.out.println("Total wage for company number "+(company_num+1)+" is "+total_wage_of_the_company);
-		
+		System.out.println("Total wage for google : "+mainobject.getTotalWage("google"));
 		
 		
 		
--- src/com/capgemini/IComputeEmpWage.java
+++ src/com/capgemini/IComputeEmpWage.java
@@ -3,5 +3,6 @@ package com.capgemini;
 public interface IComputeEmpWage {
   public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
   public void computeEmpWage(int type);
+  public int getTotalWage(String company);
   
 }
