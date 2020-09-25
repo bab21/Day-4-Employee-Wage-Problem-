852f799a0d710badd7932c01e3a4a74f0d434581
bab21 <2016061@iiitdmj.ac.in>
2020-09-25 14:53:19 +0530

    interface added

---
+++ src/com/capgemini/IComputeEmpWage.java
@@ -0,0 +1,9 @@
+package com.capgemini;
+
+public interface IComputeEmpWage {
+	
+	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
+	public void computeEmpWage();
+	
+
+}
